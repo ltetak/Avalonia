d8ec71e9ca7efb54d0cbba2b28a51186855b5fd6
Andrey Kunchev <donandren@gmail.com>
2020-10-16 13:34:33 +0300

    tmp: add emulation of combo bug issue:4875

--- samples/VirtualizationDemo/MainWindow.xaml.cs
+++ samples/VirtualizationDemo/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
+using System.Linq;
 using Avalonia;
+using Avalonia.Collections;
 using Avalonia.Controls;
+using Avalonia.Data;
 using Avalonia.Markup.Xaml;
+using ReactiveUI;
 using VirtualizationDemo.ViewModels;
 
 namespace VirtualizationDemo
@@ -12,6 +16,37 @@ namespace VirtualizationDemo
             this.InitializeComponent();
             this.AttachDevTools();
             DataContext = new MainWindowViewModel();
+
+            {
+                //bug in combobox
+                var items = new AvaloniaList<string>(Enumerable.Range(1, 5).Select(i => $"Item {i}"));
+                var cbo = this.Get<ComboBox>("cbo");
+                cbo.Items = items;
+               // var sel = new Sel();
+              //  cbo.Bind(ComboBox.SelectedItemProperty, new Binding("Selected") { Mode = BindingMode.TwoWay, Source = sel });
+                int c = 1;
+                // sel.Selected = items.First();
+                cbo.SelectedItem = items.First();
+                this.Get<Button>("btnCbo").Click += (s, e) =>
+                {
+                    c++;
+                    //sel.Selected = null;
+                    items.Clear();
+                    //items.AddRange(Enumerable.Range(1, 5).Select(i => $"New {c} Item {i}"));
+                    //cbo.Items = items.ToArray();
+                };
+            }
+        }
+
+        class Sel : ReactiveObject
+        {
+            private string _Selected;
+
+            public string Selected
+            {
+                get => _Selected;
+                set => this.RaiseAndSetIfChanged(ref _Selected, value);
+            }
         }
 
         private void InitializeComponent()
