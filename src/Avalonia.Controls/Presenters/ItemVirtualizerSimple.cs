cc24425b6a17352b31fb787d4ce03b766fb2075f
Andrey Kunchev <donandren@gmail.com>
2022-08-02 18:31:57 +0300

    tmp: issue:8669 fixed, pr:8670

--- src/Avalonia.Controls/Presenters/ItemVirtualizerSimple.cs
+++ src/Avalonia.Controls/Presenters/ItemVirtualizerSimple.cs
@@ -185,7 +185,7 @@ namespace Avalonia.Controls.Presenters
                         break;
 
                     case NotifyCollectionChangedAction.Remove:
-                        if ((e.OldStartingIndex >= FirstIndex && e.OldStartingIndex < NextIndex) ||
+                        if (e.OldStartingIndex < NextIndex ||
                             panel.Children.Count > ItemCount)
                         {
                             RecycleContainersOnRemove();
